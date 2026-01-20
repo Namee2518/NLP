41e64c3c053d19e2816f38f45324688036d04c7d
Namee2518 <nameetha1810@gmail.com>
2026-01-20 14:28:28 +0530

    Create EX2.py

---
+++ EX2.py
@@ -0,0 +1,10 @@
+
+def tokenize_text(text):
+    tokens = text.split()
+    return tokens
+
+text = "Natural Language Processing using Python"
+result = tokenize_text(text)
+
+print("Tokens are:")
+print(result)
